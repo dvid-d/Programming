2a9b0d42bd6e2d2abfe5cb4f87bdf4315b4d87f2
dvid-d <113169661+dvid-d@users.noreply.github.com>
2022-09-25 17:14:29 +0100

    Update richter.py

--- Brampton Manor Academy/Programming Challenge 2/richter.py
+++ Brampton Manor Academy/Programming Challenge 2/richter.py
@@ -1,11 +1,12 @@
 richter = [["1",1], ["5",5], ["indonesia_04",9.1], ["alaksa_64",9.2], ["chile_60",9.5]]
 
-print("Richter " + " "*15 + "Joules " + " "*15 + "TNT" )
-for i in range(len(richter)):
-    exponent = 1.5 * (richter[i][1]) + 4.8
-    energy = 10**(exponent)
-    TNT = energy / (4.184 * 10**9)
-    print(str(richter[i][1]) + " "*15 + str(energy) + " "*10 + str(TNT))
+def calc_richt_info():
+    print("Richter " + " "*15 + "Joules " + " "*15 + "TNT" )
+    for i in range(len(richter)):
+        exponent = 1.5 * (richter[i][1]) + 4.8
+        energy = 10**(exponent)
+        TNT = energy / (4.184 * 10**9)
+        print(str(richter[i][1]) + " "*15 + str(energy) + " "*10 + str(TNT))
 
 def calc_user_richt():
     user_Richter = float(input("Please enter a Richter scale value: "))
@@ -15,3 +16,7 @@ def calc_user_richt():
     tnt_user = energy_user / (4.184 * 10**9)
     print(f"Equivalence in joules: {energy_user}")
     print(f"Equivalence in tons of TNT: {tnt_user}")
+
+if __name__ == '__main__':
+    calc_richt_info()
+    calc_user_richt()
