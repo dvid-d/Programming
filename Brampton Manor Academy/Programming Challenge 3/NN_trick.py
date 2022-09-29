9ce503a0d692d04656c18828ec8a09313724ffa3
dvid-d <113169661+dvid-d@users.noreply.github.com>
2022-09-29 18:09:48 +0100

    Update NN_trick.py

--- Brampton Manor Academy/Programming Challenge 3/NN_trick.py
+++ Brampton Manor Academy/Programming Challenge 3/NN_trick.py
@@ -1,8 +1,13 @@
 number = int(input("Input a number 10-49: "))
 player_number = int(input("Enter a number 50-99: "))
 
-factor = 99 - number
-new_number = player_number + factor
-new_number = new_number - 100 + 1
-guess_number = player_number - new_number
-print(f"I guessed the answer was {number} and the calculation result was {guess_number}")
+def calculation(number, player_number):
+    factor = 99 - number
+    new_number = player_number + factor
+    new_number = new_number - 100 + 1
+    guess_number = player_number - new_number
+    print(f"I guessed the answer was {number} and the calculation result was {guess_number}")
+    return guess_number
+
+if __name__ == '__main__':
+    calculation(number, player_number)
