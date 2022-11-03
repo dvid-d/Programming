8e5c029eee1bfe026901bd9dbe60229f33406eb9
dvid-d <113169661+dvid-d@users.noreply.github.com>
2022-11-03 19:28:23 +0000

    Add files via upload

---
+++ Brampton Manor Academy/Java/Calculator.java
@@ -0,0 +1,54 @@
+import java.lang.Math;
+public class Calculator {
+
+    private static float add(int a, int b) {
+        return a + b;
+    }
+    private static float subtract(int a, int b) {
+        return a - b;
+    }
+
+    private static float multiply(int a, int b){
+        return a * b;
+    }
+
+    private static float divide(float a, float b){
+        return a / b;
+    }
+
+    private static double square(int a){
+        return Math.pow(a,2);
+    }
+
+    private static double cube(int a){
+        return Math.pow(a,3);
+    }
+
+    private static double power(int a, int b){
+        return Math.pow(a,b);
+    }
+
+    public static void main(String[] args) {
+        String method = args[0];
+        int a = Integer.parseInt(args[1]);
+        int b = Integer.parseInt(args[2]);
+        System.out.println("Please enter one of the operators below (word not symbol) and number(s) you want to use.");
+        System.out.println("Operations: add (a+b)");
+        System.out.println("Operations: subtract (a-b)");
+        System.out.println("Operations: multiply (a*b)");
+        System.out.println("Operations: divide (a/b)");
+        System.out.println("Operations: square (a^2)");
+        System.out.println("Operations: cube (a^3)");
+        System.out.println("Operations: power (a^b)");
+        switch (method) {
+            case "add" -> System.out.println(add(a, b));
+            case "subtract" -> System.out.println(subtract(a, b));
+            case "multiply" -> System.out.println(multiply(a, b));
+            case "divide" -> System.out.println(divide(a, b));
+            case "square" -> System.out.println(square(a));
+            case "cube" -> System.out.println(cube(a));
+            case "power" -> System.out.println(power(a, b));
+            default -> System.out.print("Please enter a valid operator and/or numbers.");
+        }
+    }
+}
