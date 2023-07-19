403d196c202711731eb620e4172d863a56c619b0
David <ddobre31@gmail.com>
2023-07-19 22:46:47 +0100

    test #3

--- Brampton Manor Academy/2D London Underground Simulator/Button.py
+++ Brampton Manor Academy/2D London Underground Simulator/Button.py
@@ -1,2 +1,8 @@
-class buttons():
-    pass
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.shape = self.image.get_rect()
+        self.shape.topleft = (x, y)
+
+    def Draw(self):
+        screen.blit(self.image, (self.shape.x, self.shape.y))
