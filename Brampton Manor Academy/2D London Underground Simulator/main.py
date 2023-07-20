b4c4928ad8b0b387408098b7097cf3ad56f10394
dvd-d <ddobre31@gmail.com>
2023-07-20 17:17:01 +0100

    window size change

--- Brampton Manor Academy/2D London Underground Simulator/main.py
+++ Brampton Manor Academy/2D London Underground Simulator/main.py
@@ -7,8 +7,8 @@ pygame.init()
 
 
 def MainMenu():
-    screen_width = 1920
-    screen_height = 1080
+    screen_width = 960
+    screen_height = 540
     screen = pygame.display.set_mode((screen_width,screen_height))
     pygame.display.set_caption("2D London Underground Simulator")
     pygame.display.update()
@@ -22,6 +22,9 @@ def Play():
 if __name__ == '__main__':
     run = True
     screen_width, screen_height, screen = MainMenu()
+    pygame.display.set_caption('2D London Underground Simulator')
+    screen.fill((58,208,241))
+    pygame.display.set_icon(Icon_name)
     while run:
         Play()
     pygame.quit()
