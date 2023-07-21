1bd22ae370695cd4cd275ab5e0adede983e408a3
dvd-d <ddobre31@gmail.com>
2023-07-21 19:27:31 +0100

    Added: quit button button, quit and play buttons functionality, 'lora' font and font folder

--- Brampton Manor Academy/2D London Underground Simulator/Button.py
+++ Brampton Manor Academy/2D London Underground Simulator/Button.py
@@ -1,26 +1,28 @@
 import pygame
-#[0] - left mouse button
-#[1] - middle mouse button
-#[2] - right mouse button
+#[0] - [0] left mouse button
+#[1] - [1] middle mouse button
+#[2] - [2] right mouse button
 
 
 class Button():
-    def __init__(self, x, y, image, scale):
+    def __init__(self, surface, x, y, image, scale):
         width = image.get_width()
         height = image.get_height()
         self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
-        self.shape = self.image.get_rect()
-        self.shape.topleft = (x, y)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
         self.isClicked = False
+        surface.blit(self.image, (x, y))
+        
 
-    def Draw(self, surface):
+    def wasClicked(self):
 
         mouse_position = pygame.mouse.get_pos()
+
         if self.rect.collidepoint(mouse_position):
             if pygame.mouse.get_pressed()[0] == 1 and self.isClicked == False:
                 self.isClicked = True
-        
-        if pygame.mouse.get_pressed()[0]:
-            self.isClicked = False
+            elif pygame.mouse.get_pressed()[0] == 0:
+                self.isClicked = False
 
-        surface.blit(self.image, (self.shape.x, self.shape.y))
+        return self.isClicked
