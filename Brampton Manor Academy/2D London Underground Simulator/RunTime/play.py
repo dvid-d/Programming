1592ff81af1def22dc24829566330a9138d259fe
David <ddobre31@gmail.com>
2024-01-11 18:29:12 +0000

    removed part of subroutine

--- Brampton Manor Academy/2D London Underground Simulator/RunTime/play.py
+++ Brampton Manor Academy/2D London Underground Simulator/RunTime/play.py
@@ -5,9 +5,16 @@ sys.path.append("C:\\Users\\ddobr\\Desktop\\Sixth Form\\Computer Science\\Github
 sys.path.append("C:\\Users\\ddobr\\Desktop\\Sixth Form\\Computer Science\\Github\\Programming\\Brampton Manor Academy\\2D London Underground Simulator\\RunTime")
 sys.path.append("C:\\Users\\ddobr\\Desktop\\Sixth Form\\Computer Science\\Github\\Programming\\Brampton Manor Academy\\2D London Underground Simulator\\Saves")
 sys.path.append("C:\\Users\\ddobr\\Desktop\\Sixth Form\\Computer Science\\Github\\Programming\\Brampton Manor Academy\\2D London Underground Simulator\\Trains")
-
-import pygame, settings, main, button, gameState, tile, shop, train
+PLAYER = 0
+import pygame
+from shop import *
+from button import *
+from train import *
+from tile import *
+from game import *
+import settings
 from pytmx.util_pygame import load_pygame
+
 
 class Play():
     def Load(path, screen, save_data, SCREEN_WIDTH, SCREEN_HEIGHT, run):
@@ -23,36 +30,11 @@ class Play():
             if object.type[:5] == "Track":
                 track = object
                 direction = track.type[5:7]
-                points = [(point.x, point.y) for point in track.points]  #points go clockwise from the bottom right
-                pygame.draw.polygon(screen, (100,100,100), points, 1)
-                if direction == "SB":
-                    tracks.append([track.type[-3:], "SB", points])
-                elif direction == "NB":
-                    tracks.append([track.type[-3:], "NB", points])
 
-            if object.type[:7] == "Station":
-                station = object
-                direction = station.type[7:9]
-                pygame.draw.polygon(screen, (100,100,100), station.points, 1)
-                
-                for i in range(len(lines)):
-                    if lines[i] == station.type[-3:]:
-                        stations = Play.AddStationsToList(stations, direction, i, station) #must line up with order of stations in trainLocations in save file
-        trains = [] #temp
-        if run == 1 and (int(save_data["level"]) == 0 or int(save_data["level"]) == 1):
-            location = (620,1000)                      
-            player = Play.CreatePlayer(path, screen, location)
-            return player, trains #, track_surface
+        
+        #trains.append(player)
         #also add Trains and other things
-        else:
-            return trains #, track_surface
-
-    def AddStationsToList(stations, direction, line_no, station):
-        if direction == "NB":
-            stations[line_no][1].append(station.points)
-        elif direction == "SB":
-            stations[line_no][2].append(station.points)
-        return stations
+        return 0
 
     def LoadMap(path, save_data, screen):
         map = Play.GetMap(save_data)
@@ -60,10 +42,9 @@ class Play():
         sprite_group = pygame.sprite.Group()
         layers = map_data.visible_layers
         for layer in layers:
-            if layer.name == "Tile Layer 1": #replace with layer name
-                for x,y,surface in layer.tiles():
-                    pos = (x*192,y*108) # size of tiles
-                    tile.Tile(pos, surface, sprite_group)
+            for x,y,surface in layer.tiles():
+                pos = (x*9,y*9) # size of tiles
+                Tile(pos, surface, sprite_group)
         #for object in map_data.visible_object_groups():
             #print(object)
         sprite_group.draw(screen)
@@ -77,7 +58,7 @@ class Play():
             icon_location = f"{path}\\Icons\\{line}.png"
             for obj in trains:
                 train_location = trains[obj]
-                train.Train.Display(screen, icon_location, train_location)
+                Train.Display(screen, icon_location, train_location)
 
     def CheckForRewards():
         pass
@@ -107,19 +88,14 @@ class Play():
         #     map = "level_10"
         return map
 
-    def CheckIfClicked(buttons, screen, game_settings, path):
+    def CheckButtons(buttons, screen, game_settings, path):
             if buttons[0].wasClicked():
                 game_settings.InGameSettings(screen, game_settings, path)
             # if buttons[1].wasClicked():
             #     shop.Shop.UseShop()
             #     return True
-    
-    def StartTutorial(save_data):
-        pass
-        save_data[3] = 2.0
-        return save_data
         
-    def CheckThreshold(save_data):
+    def CheckThreshold(save_data): #change, save_data variable has been changed as saves are now json files
         money = save_data[-3]
         debt = save_data[-2]
         overall_money = money - debt
@@ -143,39 +119,50 @@ class Play():
 
     def CreatePlayer(path, screen, location):
         image_location = f"{path}\\Icons\\train.png"
-        player = train.PlayerTrain(screen, "NB", "Victoria", "Player", 100, image_location, location) 
+        player = PlayerTrain(screen, "NB", "Victoria", "Player", 100, image_location, location) 
         return player
+
 
     def LoadEntities(path, screen, location):
         Play.CreatePlayer(path, screen, location)
         return 0 #to be changed to a list of all trains, grouped based on lines
 
-    def Run(screen, path, save_data, SCREEN_WIDTH, SCREEN_HEIGHT, game):
-        game_settings = settings.Settings(100, 3)
+
+    def LoadIcons(path):
         settings_button_icon = pygame.image.load(f"{path}\\Icons\\cog.png")
-        #shop_button_icon = pygame.image.load(f"{path}\\icons\\shop_button.png")
+        shop_button_icon = pygame.image.load(f"{path}\\icons\\shop_button.png")
+        return settings_button_icon, shop_button_icon
+
+    def LoadButtons(path, screen, SCREEN_WIDTH, SCREEN_HEIGHT):
+        settings_button_icon, shop_button_icon = Play.LoadIcons(path)
+        settings_button = Button(screen, SCREEN_WIDTH/20 - 70, SCREEN_HEIGHT/20 - 30, settings_button_icon, 1/(4.5)) #creates settings button
+        #shop_button = button.Button(screen, 500, 1000, shop_button_icon, 1) #creates shop button
+        return settings_button#, shop_button
+
+    def Tutorial(screen, trains):
+        trains[PLAYER].Move(screen)
+
+    
+    def Run(screen, path, save_data, SCREEN_WIDTH, SCREEN_HEIGHT, game):
+        game_settings = settings.Settings(100, 3)        
+        trains = []
 
         run = 1 #used to check if it the first time the loop is run in order to not load the player in their default position more than once (which is when first loading the map)
         while game.state == 5:
-            #shop_button = button.Button(screen, 500, 1000, shop_button_icon, 1) #creates shop button
-            if run == 1:
-                player, trains = Play.Load(path, screen, save_data, SCREEN_WIDTH, SCREEN_HEIGHT, run)
-                run = 0
-            else:
-                trains = Play.Load(path, screen, save_data, SCREEN_WIDTH, SCREEN_HEIGHT, run)
-            settings_button = button.Button(screen, SCREEN_WIDTH/1.17 - 1325, SCREEN_HEIGHT/6-10, settings_button_icon, 1/(4.5)) #creates settings button
-            buttons = [settings_button] #, shop_button etc #list of buttons to loop through and proceed with their individual actions if clicked
-            Play.CheckIfClicked(buttons, screen, game_settings, path)
+            stations = Play.Load(path, screen, save_data, SCREEN_WIDTH, SCREEN_HEIGHT, run)
+            run = 0
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        game_settings.InGameSettings(screen, game_settings, path)
-                        clicked = True
-            player.Move(screen)
+            settings_button = Play.LoadButtons(path, screen, SCREEN_WIDTH, SCREEN_HEIGHT)
+            buttons = [settings_button] #, shop_button etc #list of buttons to loop through and proceed with their individual actions if clicked
+            Play.CheckButtons(buttons, screen, game_settings, path)
+
+                        # clicked = True
+            # for train in trains:
+            #     for track in tracks:
+            #         if tracks[track][0][:3] == train.GetLine() and tracks[track][1] == train.GetDirection():
+            #             track_points = tracks[track][2]
+            #if int(save_data["level"]) == 0 or int(save_data["level"]) == 1:
+                #trains[PLAYER].Move(screen) #track_points
 
             # for train in trains:
             #     train.Move()
@@ -184,4 +171,11 @@ class Play():
             #check to see if next threshold has been met;
             #Play.CheckLevel()
             #Check to see if any train has met the end of their line
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        game_settings.InGameSettings(screen, game_settings, path)
             pygame.display.update()
