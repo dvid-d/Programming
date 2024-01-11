82b506486e0be357bcc823c7b3056dc70a547f3a
David <ddobre31@gmail.com>
2024-01-11 18:34:15 +0000

    changed importing

--- Brampton Manor Academy/2D London Underground Simulator/Game Properties/settings.py
+++ Brampton Manor Academy/2D London Underground Simulator/Game Properties/settings.py
@@ -12,8 +12,8 @@ import pygame, button, main, shop
 
 class Settings():
     def __init__(self, volume, difficulty):
-        self.volume = volume
-        self.difficulty = difficulty
+        self.__volume = volume
+        self.__difficulty = difficulty
 
     def InGameSettings(self, screen, game_settings, path):
         inSettings = True
@@ -28,9 +28,6 @@ class Settings():
                 pygame.display.update()
             
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         inSettings = False
@@ -38,17 +35,17 @@ class Settings():
             pygame.display.update()
 
     def Display(self,screen, path):
-        background = pygame.Rect(400,250,1000,700)
+        background = pygame.Rect(300,100,1000,700)
         pygame.draw.rect(screen, (255,255,255), background)
 
-        border = pygame.Rect(300,250,1000,700)
+        border = pygame.Rect(300,100,1000,700)
         pygame.draw.rect(screen,(0,0,0),border,2)
 
         quit_button_icon = pygame.image.load(f'{path}\\icons\\quit_button.png')
-        quit_button = button.Button(screen, 700, 840, quit_button_icon, 0.35)
+        quit_button = button.Button(screen, 700, 700, quit_button_icon, 0.35)
 
         back_button_icon = pygame.image.load(f"{path}\\icons\\back_button.png")
-        back_button = button.Button(screen, 1190, 260, back_button_icon, 0.2)
+        back_button = button.Button(screen, 1190, 110, back_button_icon, 0.2)
 
 
 
