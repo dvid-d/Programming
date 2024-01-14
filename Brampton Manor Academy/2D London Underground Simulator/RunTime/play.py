b9c617465eb0d5499448ae990e6ce4c654980a9a
David <ddobre31@gmail.com>
2024-01-14 17:52:06 +0000

    +groundwork code for train/station objs to be used

--- Brampton Manor Academy/2D London Underground Simulator/RunTime/play.py
+++ Brampton Manor Academy/2D London Underground Simulator/RunTime/play.py
@@ -45,10 +45,33 @@ class Play():
             for x,y,surface in layer.tiles():
                 pos = (x*9,y*9) # size of tiles
                 Tile(pos, surface, sprite_group)
+        #Tile.test()
         #for object in map_data.visible_object_groups():
             #print(object)
         sprite_group.draw(screen)
         return map_data
+    
+    def CreatePlayer(path, screen, location):
+        image_location = f"{path}\\Icons\\train.png"
+        player = PlayerTrain(screen, "NB", "Victoria", "Player", 100, image_location, location) 
+        return player
+
+
+    def LoadEntities(path, screen, location):
+        Play.CreatePlayer(path, screen, location)
+        return 0 #to be changed to a list of all trains, grouped based on lines
+
+
+    def LoadIcons(path):
+        settings_button_icon = pygame.image.load(f"{path}\\Icons\\cog.png")
+        shop_button_icon = pygame.image.load(f"{path}\\icons\\shop_button.png")
+        return settings_button_icon, shop_button_icon
+
+    def LoadButtons(path, screen, SCREEN_WIDTH, SCREEN_HEIGHT):
+        settings_button_icon, shop_button_icon = Play.LoadIcons(path)
+        settings_button = Button(screen, SCREEN_WIDTH/1.17 - 1325, SCREEN_HEIGHT/6-10, settings_button_icon, 1/(4.5)) #creates settings button
+        #shop_button = button.Button(screen, 500, 1000, shop_button_icon, 1) #creates shop button
+        return settings_button#, shop_button
     
     def LoadTrains(path, screen, lines):
         #lines[Line][Train]
@@ -142,28 +165,36 @@ class Play():
     def Tutorial(screen, trains):
         trains[PLAYER].Move(screen)
 
+    def CreateTrains():
+        pass
     
     def Run(screen, path, save_data, SCREEN_WIDTH, SCREEN_HEIGHT, game):
-        game_settings = settings.Settings(100, 3)        
-        trains = []
+        game_settings = settings.Settings(100, 3)
+
+        count = 0
+        trains = save_data["trainLocations"] #is a dictionary
+
+        level_matrix = Path.LoadMatrix("level_1", path)
+        TileIDsVic = [0,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,320,340,350,360,370,380,390,400,410,367,377,397,398] #Allowed IDs only; i.e. can only pass over these tiles
+        for row in level_matrix:
+            for tile in row:
+                if tile == 120: #Tile ID 120 is Northnound starting positoon for Victoria Line
+                    train = Train("SB","Victoria", 100, f"{path}\\Icons\\train.png", (row, tile), 120)
+                    trains.append(train, (row,tile))
 
         run = 1 #used to check if it the first time the loop is run in order to not load the player in their default position more than once (which is when first loading the map)
         while game.state == 5:
-            stations = Play.Load(path, screen, save_data, SCREEN_WIDTH, SCREEN_HEIGHT, run)
             run = 0
 
             settings_button = Play.LoadButtons(path, screen, SCREEN_WIDTH, SCREEN_HEIGHT)
             buttons = [settings_button] #, shop_button etc #list of buttons to loop through and proceed with their individual actions if clicked
             Play.CheckButtons(buttons, screen, game_settings, path)
 
-                        # clicked = True
-            # for train in trains:
-            #     for track in tracks:
-            #         if tracks[track][0][:3] == train.GetLine() and tracks[track][1] == train.GetDirection():
-            #             track_points = tracks[track][2]
-            #if int(save_data["level"]) == 0 or int(save_data["level"]) == 1:
-                #trains[PLAYER].Move(screen) #track_points
+            if count % 40:
+                Play.CreateTrains()
 
+            
+                        # clicked = True
             # for train in trains:
             #     train.Move()
                                         
@@ -178,4 +209,5 @@ class Play():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         game_settings.InGameSettings(screen, game_settings, path)
+            count += 1
             pygame.display.update()
