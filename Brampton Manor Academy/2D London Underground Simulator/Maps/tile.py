062aeb41530a49ba48f8a878364b4ae60db59cc8
David <ddobre31@gmail.com>
2024-01-14 18:07:09 +0000

    changed tile class in attempt to fix bug

--- Brampton Manor Academy/2D London Underground Simulator/Maps/tile.py
+++ Brampton Manor Academy/2D London Underground Simulator/Maps/tile.py
@@ -1,7 +1,15 @@
 import pygame
 
 class Tile(pygame.sprite.Sprite):
+    global temp 
+    temp = []
     def __init__(self, pos, surface, groups):
         super().__init__(groups)
         self.image = surface
+        #print(pos)
         self.rect = self.image.get_rect(topleft = pos)
+        # except:
+        #     temp.append(pos)
+
+    def test():
+        print(temp)
