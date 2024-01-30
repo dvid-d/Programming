e2bfef7d86a9c8cec7be4fbf90ed7618fd58d3b4
dvid-d <113169661+dvid-d@users.noreply.github.com>
2024-01-30 14:05:53 +0000

    Add files via upload

---
+++ Brampton Manor Academy/2D London Underground Simulator/Modified path.append.py
@@ -0,0 +1,42 @@
+# main.py
+
+global path
+path = abspath(getsourcefile(lambda:0))[:-16] # obtains path of program
+#import sys
+sys.path.append(f"{path}\\Game Properties")
+sys.path.append(f"{path}\\Fonts")
+sys.path.append(f"{path}\\Maps")
+sys.path.append(f"{path}\\RunTime")
+sys.path.append(f"{path}\\Saves")
+sys.path.append(f"{path}\\Trains")
+
+#play.py
+
+#import sys
+sys.path.append(f"{path}\\Game Properties")
+sys.path.append(f"{path}\\Fonts")
+sys.path.append(f"{path}\\Maps")
+sys.path.append(f"{path}\\RunTime")
+sys.path.append(f"{path}\\Saves")
+sys.path.append(f"{path}\\Trains")
+
+#shop.py
+
+#import pygame, sys, button
+sys.path.append(f"{path}\\Game Properties")
+sys.path.append(f"{path}\\Fonts")
+sys.path.append(f"{path}z\Maps")
+sys.path.append(f"{path}\\RunTime")
+sys.path.append(f"{path}\\Saves")
+sys.path.append(f"{path}\\Trains")
+
+#settings.py
+
+#import sys
+
+sys.path.append(f"{path}\\Game Properties")
+sys.path.append(f"{path}\\Fonts")
+sys.path.append(f"{path}\\Maps")
+sys.path.append(f"{path}\\RunTime")
+sys.path.append(f"{path}\\Saves")
+sys.path.append(f"{path}\\Trains")
