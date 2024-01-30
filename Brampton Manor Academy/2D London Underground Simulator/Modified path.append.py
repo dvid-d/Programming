a3f514155a20dd40ba5826aef28871d2d138fc80
dvid-d <113169661+dvid-d@users.noreply.github.com>
2024-01-30 14:09:49 +0000

    Update Modified path.append.py
    Moved up some imports

--- Brampton Manor Academy/2D London Underground Simulator/Modified path.append.py
+++ Brampton Manor Academy/2D London Underground Simulator/Modified path.append.py
@@ -1,14 +1,27 @@
 # main.py
 
+import sys
+from os.path import abspath
+from inspect import getsourcefile
+
 global path
 path = abspath(getsourcefile(lambda:0))[:-16] # obtains path of program
-#import sys
 sys.path.append(f"{path}\\Game Properties")
 sys.path.append(f"{path}\\Fonts")
 sys.path.append(f"{path}\\Maps")
 sys.path.append(f"{path}\\RunTime")
 sys.path.append(f"{path}\\Saves")
 sys.path.append(f"{path}\\Trains")
+
+import pygame
+import time
+from pytmx.util_pygame import load_pygame
+from button import *
+from controls import *
+from settings import *
+from play import *
+from game import *
+from saves import *
 
 #play.py
 
