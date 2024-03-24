2686f3ff714c18775bad48b8c97f87662cf198dc
dvid-d <ddobre31@gmail.com>
2024-03-24 18:04:57 +0000

    moved new subroutines in

--- Brampton Manor Academy/2D London Underground Simulator/Game Properties/settings.py
+++ Brampton Manor Academy/2D London Underground Simulator/Game Properties/settings.py
@@ -2,6 +2,7 @@ import sys
 
 from os.path import abspath
 from inspect import getsourcefile
+from button import *
 path = abspath(getsourcefile(lambda:0))[:-16]
 
 sys.path.append(f"{path}\\Game Properties")
@@ -12,39 +13,49 @@ sys.path.append(f"{path}\\Saves")
 sys.path.append(f"{path}\\Trains")
 
 
-import pygame, button, main, shop
+import pygame, main
 
 class Settings():
     def __init__(self, volume, difficulty):
         self.__volume = volume
         self.__difficulty = difficulty
 
-    def InGameSettings(self, screen, game_settings, path):
-        inSettings = True
-        while inSettings is True:
-            quit_button, back_button = game_settings.Display(screen, path) #add shop_button
+    def InGameSettings(screen, path, isInSettings):
+        while isInSettings:
+            quit_button, back_button = Settings.Display(screen, path) #add shop_button
             if back_button.wasClicked():
-                inSettings = False
+                isInSettings = False
             elif quit_button.wasClicked():
                 #ask user if they want to save
                 #save if they want to
+                isInSettings = False
                 main.Quit()
                 pygame.display.update()
-            """
-            elif help_button.wasClicked():
-                load help section
+            # """
+            # elif help_button.wasClicked():
+            #     load help section
                 
-            elif map_key.wasClicked():
-                Load map key
-                """
+            # elif map_key.wasClicked():
+            #     Load map key
+            #     """
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
-                        inSettings = False
-                    
+                        isInSettings = False
             pygame.display.update()
+        pygame.display.update()
+        return isInSettings
+    
+    def loadIcons(path):
+        settings_button_icon = pygame.image.load(f"{path}\\Icons\\cog.png")
+        return settings_button_icon
 
-    def Display(self,screen, path):
+    def loadButtons(surface, path, SCREEN_WIDTH, SCREEN_HEIGHT):
+        settings_button_icon = Settings.loadIcons(path)
+        settings_button = Button(surface, SCREEN_WIDTH/20 - 70, SCREEN_HEIGHT/20 - 30, settings_button_icon, 1/(4.5)) #creates settings button
+        return settings_button  
+
+    def Display(screen, path):
         background = pygame.Rect(300,100,1000,700)
         pygame.draw.rect(screen, (255,255,255), background)
 
@@ -52,12 +63,16 @@ class Settings():
         pygame.draw.rect(screen,(0,0,0),border,2)
 
         quit_button_icon = pygame.image.load(f'{path}\\icons\\quit_button.png')
-        quit_button = button.Button(screen, 700, 700, quit_button_icon, 0.35)
+        quit_button = Button(screen, 700, 700, quit_button_icon, 0.35)
 
         back_button_icon = pygame.image.load(f"{path}\\icons\\back_button.png")
-        back_button = button.Button(screen, 1190, 110, back_button_icon, 0.2)
+        back_button = Button(screen, 1190, 110, back_button_icon, 0.2)
+        return quit_button, back_button
 
-
+    def checkButtons(buttons, surface, path):
+        if buttons[0].wasClicked():
+            isInSettings = True
+            Settings.InGameSettings(surface, path, isInSettings)
+            buttons[0].setClicked(False)
 
         #controls sub section
-        return quit_button, back_button
