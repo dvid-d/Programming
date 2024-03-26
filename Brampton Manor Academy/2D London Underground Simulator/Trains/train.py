12d81fa8ca63e66b56dc433302c3bde41011e09a
dvid-d <113169661+dvid-d@users.noreply.github.com>
2024-03-26 09:33:07 +0000

    added "time to complete event" property

--- Brampton Manor Academy/2D London Underground Simulator/Trains/train.py
+++ Brampton Manor Academy/2D London Underground Simulator/Trains/train.py
@@ -58,6 +58,7 @@ class Train(pygame.sprite.Sprite):
         self.__speed = speed
         self.__isAtStation = False
         self.__stop_timer = 0
+        self.__time_to_complete_event = 0
         self.__max_capacity = 0
         self.__number_of_passengers = 0
 
