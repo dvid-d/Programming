e65197ce8cc67303a90e37783ac043da450a54cf
dvid-d <113169661+dvid-d@users.noreply.github.com>
2024-03-26 14:41:37 +0000

    Add files via upload

--- Brampton Manor Academy/2D London Underground Simulator/Events/event.py
+++ Brampton Manor Academy/2D London Underground Simulator/Events/event.py
@@ -1,5 +1,16 @@
+import pygame
+
+import json
+from inspect import getsourcefile
+from os.path import abspath
+path = abspath(getsourcefile(lambda:0))[:-16]
+
 class Event():
-  
+
+  events = []
+  with open(f"{path}\\Saves\\event_descriptions.json", 'r') as save_file:
+            events = json.load(save_file)
+
   def __init__(self, id, line, train):
     self.__ID = id
     self.__line = line
@@ -17,17 +28,34 @@ class Event():
     #8:"Fire alert", 9:"Flooding"
     #Fire alert affects service less
     #Flooding more severe
-
-  def Breakdown(self):
     pass
+  
+  def Display_Options(screen, option_A, option_B):
+     pass
+  
+  def Breakdown(self, events, save_data):
+    train = self.__train
+    train.set_isBrokenDown = True
 
-#Trains breaking down
-# Track faults
-# Closed stations
-# Signal issues (e.g. “signalling upgrades are needed but there is not enough funding. What do you do? {ignore} / {borrow money} / {Repair signal infrastructure}”)
-# Flooding
-# Crime
-# Lack of available trains/drivers
-# Driver Strikes
-# Train getting too hot
-# Fire alert
+    repair_cost_A = events["Breakdown"]["A"]["Cost"]
+    time_to_solve_A = events["Breakdown"]["A"]["Duration"]
+
+    repair_cost_B = events["Breakdown"]["B"]["Cost"]
+    time_to_solve_B = events["Breakdown"]["B"]["Duration"]
+
+    print(f"Train ({train.getID()}) on {train.getLine().capitalize()} line has broken down.")
+    print("Choose one of the options below to resolve this.")
+    print()
+    print(f"A: Repair train (£{repair_cost_A}, {time_to_solve_A}s)")
+    print(f"B: Wait for engineers (£{repair_cost_B}, {time_to_solve_B}s)")
+
+    decision = input().upper()
+
+    if decision == 'A':
+       new_money = save_data["money"] - repair_cost_A
+       save_data["money"] = new_money
+
+
+    elif decision == 'B':
+       ew_money = save_data["money"] - repair_cost_B
+       save_data["money"] = new_money
