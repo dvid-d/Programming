f4b09f727dc9cb32c33e30b4b55bed18bb961115
dvid-d <113169661+dvid-d@users.noreply.github.com>
2024-03-26 09:43:54 +0000

    New event properties, skeleton code for some subroutines

--- Brampton Manor Academy/2D London Underground Simulator/Events/event.py
+++ Brampton Manor Academy/2D London Underground Simulator/Events/event.py
@@ -1,8 +1,25 @@
 class Event():
-
-  def __init__(self, id):
+  
+  def __init__(self, id, line, train):
     self.__ID = id
+    self.__line = line
+    self.__isCompleted = False
+    self__train = pygame.sprite.GroupSingle(train)
+  
+  def setAsCompleted(self):
+    self.__isCompleted = True
 
+  def get_isCompleted(self):
+    return self.__isCompleted
+
+  def closeStation(self):
+    #choose randomly between 8 and 9
+    #8:"Fire alert", 9:"Flooding"
+    #Fire alert affects service less
+    #Flooding more severe
+
+  def Breakdown(self):
+    pass
 
 #Trains breaking down
 # Track faults
